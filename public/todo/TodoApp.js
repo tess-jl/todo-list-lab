f4dc8483fe3b670465e28d01718585a7014ce3a9
Tess Lameyer <tjlameyer@gmail.com>
2019-11-12 10:06:20 -0800

    todoapp seems good

--- public/todo/TodoApp.js
+++ public/todo/TodoApp.js
@@ -18,29 +18,107 @@ class TodoApp extends Component {
         dom.appendChild(loading.renderDOM());
 
         // initial todo load:
-        const addTodo = new AddTodo({
-            onAdd: async type => {
+        const addTodoClass = new AddTodo({
+            onAdd: async todo => {
                 loading.update({ loading: true });
                 // clear prior error
                 error.textContent = '';
                 try {
                     const saved = await addTodo(todo);
 
-                    const { todos }= this.state;
+                    const { todos } = this.state;
                     todos.push(saved);
 
                     //tell component to update
                     todoList.update({ todos });
                 }
                 catch (err) {
-                    console.log('TodoApp.js catch error: '+ err);
+                    console.log('TodoApp.js onAdd catch error: ' + err);
                 }
                 finally {
                     loading.update({ loading: false });
                 }
             }
         });
-        main.appendChild(addToDo.renderDOM());
+        main.appendChild(addTodoClass.renderDOM());
+
+        const todoList = new TodoList({ 
+            todos: [],
+            onUpdate: async todo => {
+                loading.update({ loading: true });
+                // clear prior error
+                error.textContent = '';
+
+                try {
+                    // part 1: do work on the server
+                    const updated = await updateTodo(todo);
+                    
+                    // part 2: integrate back into our list
+                    const { todos } = this.state;
+                    // find the index of this type:
+                    const index = todos.indexOf(todo);
+                    // replace with updated object from server:
+                    todos.splice(index, 1, updated);
+
+                    // part 3: tell component to update
+                    todoList.update({ todos });
+                }
+                catch (err) {
+                    // display error
+                    console.log('TodoApp.js onUpdate catch error: ' + err);
+                }
+                finally {
+                    loading.update({ loading: false });
+                }
+            },
+            onRemove: async todo => {
+                loading.update({ loading: true });
+                // clear prior error
+                error.textContent = '';
+
+                try {
+                    // part 1: do work on the server
+                    await removeTodo(todo.id);
+                    
+                    // part 2: integrate back into our list
+                    const { todos } = this.state;        
+                    // find the index of this type:
+                    const index = todos.indexOf(todo);
+                    // remove from the list
+                    todos.splice(index, 1);
+    
+                    // part 3: tell component to update
+                    todoList.update({ todos });
+                }
+                catch (err) {
+                    // display error
+                    console.log('TodoApp.js onRemove catch error: ' + err);
+                }
+                finally {
+                    loading.update({ loading: false });
+                }
+            }
+        });
+        main.appendChild(todoList.renderDOM());
+
+        try {
+            // get the types when this component first loads:
+            const todos = await getTodos({ showAll: true });
+            // store on "this.state" so we can get 
+            // them back for add, remove, and update:
+            this.state = { todos };
+    
+            // pass the loaded types to the component:
+            todoList.update({ todos });
+        }
+        catch (err) {
+            // display error
+            console.log('TodoApp.js general catch error: ' + err);
+        }
+        finally {
+            loading.update({ loading: false });
+        }
+
     }
 
     renderHTML() {
