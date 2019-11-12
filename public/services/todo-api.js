d78918a152910119b0fcac00b69e3151aad14ee2
Tess Lameyer <tjlameyer@gmail.com>
2019-11-12 09:09:38 -0800

    setting up with starter code

---
+++ public/services/todo-api.js
@@ -0,0 +1,43 @@
+const URL = '/api';
+
+async function fetchWithError(url, options) {
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    if (response.ok) {
+        return data;
+    }
+    else {
+        throw data.error;
+    }
+}
+
+export function getTodos() {  
+    const url = `${URL}/todos`;
+    return fetchWithError(url);
+}
+
+export function addTodo(todo) {  
+    const url = `${URL}/todos/${todo}`;
+    return fetchWithError(url);
+}
+
+export function updateTodo(todo) {  
+    const url = `${URL}/types/${todo.id}`;
+    return fetchWithError(url, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(todo)
+    });
+    
+}
+
+export function removeTodo(todoId) {  
+    const url = `${URL}/types/${todoId}`;
+    return fetchWithError(url, {
+        method: 'DELETE'
+    });
+}
+
